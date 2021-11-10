ad2d20acc1c869dcbd42151a2e32a573acb75391
vishal <vrana14693@gmail.com>
2021-11-10 20:59:05 +0530

    Adding active class using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./app.css";
-
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+
   const moneyPyramid = [
     { id: 1, amount: "$ 100" },
     { id: 2, amount: "$ 200" },
@@ -25,7 +27,13 @@ function App() {
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
-            <li className="moneyListItem ">
+            <li
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
               <span className="moneyListItemNubmer ">{m.id}</span>
               <span className="moneyListItemAmount">{m.amount}</span>
             </li>
