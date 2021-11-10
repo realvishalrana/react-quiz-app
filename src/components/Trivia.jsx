9b42d6d864ca89ce57c88b6c8f818a0e055fc618
vishal <vrana14693@gmail.com>
2021-11-11 00:19:28 +0530

    CSS animation used

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -4,7 +4,7 @@ export default function Trivia() {
       <div className="trivia">
         <div className="question">What is best Series </div>
         <div className="answers">
-          <div className="answer correct">BB ki vines</div>
+          <div className="answer wrong">BB ki vines</div>
           <div className="answer">Thugesh</div>
           <div className="answer">Neon man</div>
           <div className="answer">Harsh beniwal</div>
