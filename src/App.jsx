6a2f2d64dd5eb23b3af2837631d14a3cf4e52f4e
vishal <vrana14693@gmail.com>
2021-11-10 23:17:06 +0530

    Designing main container

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,12 @@ function App() {
 
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">question and anwers</div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
