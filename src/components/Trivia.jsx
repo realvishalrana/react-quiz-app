68c23e5001997a0ac17b8ee5c3f4b869f658c15c
vishal <vrana14693@gmail.com>
2021-11-11 01:28:07 +0530

    app logics created

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,13 +1,36 @@
-export default function Trivia() {
+import { useEffect, useState } from "react";
+
+export default function Trivia({
+  data,
+  setTimeOut,
+  questionNumber,
+  setQuestionNumber,
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectAnswer, setSelectAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const handleClick = (a) => {
+    setSelectAnswer(a);
+    setClassName("answer active");
+  };
+
   return (
     <div>
       <div className="trivia">
-        <div className="question">What is best Series </div>
+        <div className="question">{question?.question}</div>
         <div className="answers">
-          <div className="answer wrong">BB ki vines</div>
-          <div className="answer">Thugesh</div>
-          <div className="answer">Neon man</div>
-          <div className="answer">Harsh beniwal</div>
+          {question?.answers.map((a) => (
+            <div
+              className={selectAnswer === a ? className : "answer"}
+              onClick={() => handleClick(a)}
+            >
+              {a.text}
+            </div>
+          ))}
         </div>
       </div>
     </div>
