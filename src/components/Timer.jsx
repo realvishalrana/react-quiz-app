74c098b205e1e569dd5982f7927eb4e52744c0b9
vishal <vrana14693@gmail.com>
2021-11-11 14:06:45 +0530

    sound added

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -9,6 +9,8 @@ export default function Timer({ setStop, questionNumber }) {
     const interval = setInterval(() => {
       setTimer((prev) => prev - 1);
     }, 1000);
+
+    return () => clearInterval(interval);
   }, [setStop, timer]);
 
   useEffect(() => {
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../assets/play.mp3";
+import wrong from "../assets/wrong.mp3";
+import correct from "../assets/correct.mp3";
 
 export default function Trivia({
   data,
@@ -9,6 +13,14 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectAnswer, setSelectAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
+
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
@@ -25,12 +37,18 @@ export default function Trivia({
     delay(3000, () =>
       setClassName(a.correct ? "answer correct" : "answer wrong")
     );
-    delay(6000, () => {
+    delay(5000, () => {
       if (a.correct) {
-        setQuestionNumber((prev) => prev + 1);
-        setSelectAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
     // setTimeout(() => { or of delay(3000 ...)
